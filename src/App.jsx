e8c9d5c1c72950fdee3144c6f4b2c54fa40c7f55
Luigi Lupini <luigi.lupini@gmail.com>
2022-10-19 13:40:14 +0200

    #3 - `<App />` is our root component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
+/* # App is our root component: 
+App is the root component in our React application. Its the first function that
+is a component, that is called within our `index.js` script file. It renders it
+to the index.html `root` container position via `document.getElementById`.
+
+In turn App `return` that means (renders) its own content. This HTML/XML looking
+syntax is called JSX. Components/functions have to start with a capital letter.
+The `return` also has to be a single expression. The JSX syntax makes it easier
+to create HTML like templates, in the background a transpiler called Babel does
+convert all of the JSX to regular JS code for browsers to understand. Additional
+the Babel transpiler needs our `props` to be in camel cased.
+
+> React v16+, we don't need to import `React` from 'react' for each component.
+*/
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="content">
+        <h1>App Component</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
